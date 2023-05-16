c18de39272fd1cf4720a4f968efc39b045ab9807
EagleJhow <gamerjhon389@gmail.com>
2023-05-16 01:49:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TudoList from './TudoList'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TudoList />
+  </React.StrictMode>,
+)
